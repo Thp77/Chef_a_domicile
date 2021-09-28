e48b0071e73686f27989c4c1b14168eb6de7a704
Thierry7486 <thierrymalherbe@live.fr>
2021-09-28 21:36:37 +0200

    Mise en place du controller Product et affichage des produits selon le cuisinier

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -9,8 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ProductController extends AbstractController
 {
 	public function showProductsAction(ProductRepository $productRepository, Product $chief)
-	{	
-		dd($productRepository->findByChief($chief)->getContent());		
+	{				
 		return $this->render('cook/show.product.html.twig',
 		 ['products' => $productRepository->findByChief($chief)]);
 	}
