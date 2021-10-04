612b62e06fe6562ae03bce7f0076b7d739a3771a
Thp77 <denis_devillers_7@hotmail.com>
2021-10-04 14:09:20 +0200

    ajout de css sur les formulaire d'insciption/connexion/modication et mise en page de la page cuisinier

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -24,62 +24,75 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('username', null, ['label' => 'Pseudonyme'] )
-        ->add('firstname', null, ['label' => 'Prénom'] )
-        ->add('name', null, ['label' => 'Nom'] )
-        ->add('email', EmailType::class)
-        ->add('plainPassword', RepeatedType::class, [
-            'type' => PasswordType::class,
-            'mapped' => false,
-            'invalid_message' => 'The password fields must match.',
-            'required' => true,
-            'first_options'  => ['label' => 'Mot de passe',
-                                'constraints' => [
-                                new NotBlank([
-                                    'message' => 'Entrez un mot de passe',
-                                ]),
-                                new Length([
-                                    'min' => 6,
-                                    'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères',
-                                    // max length allowed by Symfony for security reasons
-                                    'max' => 255,
-                                ]),
-                            ]],
-            'second_options' => ['label' => 'Confirmez votre mot de passe'],
-        ])
-        ->add('phone', null, ['label' => 'Téléphone'] )
-        ->add('birthday', DateType::class, [
-            'label' => 'Date de naissance',
-            'widget' => 'single_text',
+            ->add('username', null, ['label' => 'Pseudonyme'])
+            ->add('firstname', null, ['label' => 'Prénom'])
+            ->add('name', null, ['label' => 'Nom'])
+            ->add('email', EmailType::class)
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'invalid_message' => 'The password fields must match.',
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe',
+                                     'attr' => ['class' => 'form-control'],
+                                     'constraints' => [
+                                          new NotBlank([
+                                            'message' => 'Entrez un mot de passe',
+                                              ]),
+                                             new Length([
+                                                'min' => 6,
+                                                'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères',
+                                                // max length allowed by Symfony for security reasons
+                                                'max' => 255,
+                                            ]),
+                                        ]
+                                    ],
+                                    'second_options' => [
+                                        'label' => 'Confirmez votre mot de passe',
+                                        'attr' => ['class' => 'form-control']
+                                    ],
+                                ])
+            ->add('phone', null, ['label' => 'Téléphone'])
+            ->add('birthday', DateType::class, [
+                'label' => 'Date de naissance',
+                'widget' => 'single_text',
             ])
-        ->add('roles', CheckboxType::class, [
-            'mapped' => false,
-            'required' => false,
-            'label' => 'Cochez si vous proposez vos services de cuisinier',
-            'attr' => ['class' => 'checkbox_user_rol'],
+            ->add('roles', CheckboxType::class, [
+                'mapped' => false,
+                'required' => false,
+                'label' => 'Cochez si vous proposez vos services de cuisinier ',
+                'attr' => ['class' => 'checkbox_user_rol '],
             ])
-        ->add('description', TextareaType::class, [
-            'required' => false,
-            'label' => 'Décrivez vous en quelques mots !',
-            'label_attr' => ['class' => 'd-none label_user_descript'],
-            'attr' => ['class' => 'd-none textarea_user_descript'],
-        ])
-        ->add('file',FileType::class, [
-            'mapped' => false,
-            'label' => 'Photo de profil',
-            'required' => false,
+            ->add('description', TextareaType::class, [
+                'required' => false,
+                'label' => 'Décrivez vous en quelques mots ! ',
+                'label_attr' => ['class' => 'd-none label_user_descript'],
+                'attr' => [
+                    'class' => 'd-none textarea_user_descript',
+                    'rows' => '10',
+                    'cols' => '25',
+                ],
             ])
-        ->add('agreeTerms', CheckboxType::class, [
-            'mapped' => false,
-            'label' => 'Accepter les termes',
-            'constraints' => [
-                new IsTrue([
-                    'message' => 'Vous devez accepter les termes',
-                ]),
-            ],
-        ])
-        ->add('submit', SubmitType::class, ['label' => 'S\'inscrire'])
-    ;
+            ->add('file', FileType::class, [
+                'mapped' => false,
+                'label' => 'Photo de profil',
+                'required' => false,
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'label' => 'Accepter les termes',
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez accepter les termes',
+                    ]),
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'S\'inscrire',
+                'attr' => [
+                    'class' => 'btn text-white mt-2',
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -22,46 +22,52 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', null, ['label' => 'Pseudonyme'] )
-            ->add('firstname', null, ['label' => 'Prénom'] )
-            ->add('name', null, ['label' => 'Nom'] )
+            ->add('username', null, ['label' => 'Pseudonyme'])
+            ->add('firstname', null, ['label' => 'Prénom'])
+            ->add('name', null, ['label' => 'Nom'])
             ->add('email', EmailType::class)
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'mapped' => false,
                 'invalid_message' => 'The password fields must match.',
                 'required' => true,
-                'first_options'  => ['label' => 'Mot de passe',
-                                    'constraints' => [
-                                    new NotBlank([
-                                        'message' => 'Entrez un mot de passe',
-                                    ]),
-                                    new Length([
-                                        'min' => 6,
-                                        'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères',
-                                        // max length allowed by Symfony for security reasons
-                                        'max' => 255,
-                                    ]),
-                                ]],
-                'second_options' => ['label' => 'Confirmez en inscrivant votre mot de passe'],
+                'first_options'  => [
+                    'label' => 'Mot de passe',
+                    'attr' => ['class' => 'form-control'],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Entrez un mot de passe',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 255,
+                        ]),
+                    ]
+                ],
+                'second_options' => ['label' => 'Confirmez en inscrivant votre mot de passe',
+                'attr' => ['class' => 'form-control'],
+            ],
             ])
-            ->add('phone', null, ['label' => 'Téléphone'] )
+
+            ->add('phone', null, ['label' => 'Téléphone'])
             ->add('birthday', DateType::class, [
                 'label' => 'Date de naissance',
                 'widget' => 'single_text',
-                ])
-            ->add('file',FileType::class, [
+            ])
+            ->add('file', FileType::class, [
                 'mapped' => false,
                 'label' => 'Photo de profil',
                 'required' => false,
-                ])
-            ->add('submit', SubmitType::class, ['label' => 'Modifier',
-            'attr' => ['class' => 'btn btn-success'],
             ])
-        ;
-        }
-            
-    
+            ->add('submit', SubmitType::class, [
+                'label' => 'Modifier',
+                'attr' => ['class' => 'btn '],
+            ]);
+    }
+
+
 
     public function configureOptions(OptionsResolver $resolver)
     {
