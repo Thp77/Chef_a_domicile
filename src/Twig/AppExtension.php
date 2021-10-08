f3406cf4553dd829270c986a28eafed289d83495
Thierry7486 <thierrymalherbe@live.fr>
2021-10-08 22:45:39 +0200

    Mise en page de quelques pages. Il reste Cook: showmenu et showproduct, Menu: show

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -16,7 +16,7 @@ class AppExtension extends AbstractExtension
 
 	public function resizeList(string $imagePath, string $alt='', string $class=''): string
 	{
-	return '<img src="' . $imagePath .'" alt="' . $alt .'" class="img-fitt py-2 ' . $class .'" style="height: 120px;"/>';
+	return '<img src="' . $imagePath .'" alt="' . $alt .'" class="img-fitt py-2 ' . $class .'"/>';
 	}
 
 	
